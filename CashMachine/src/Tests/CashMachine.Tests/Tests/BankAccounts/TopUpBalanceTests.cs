97a464f1834a7cbcfc2691f6a2f6727ac8de8b9d
fantom3527 <62473392+fantom3527@users.noreply.github.com>
2023-12-27 00:37:23 +0700

    Добавлены xUnit тесты.
    Добавлено 3 теста на снятие и пополнение денег.

---
+++ CashMachine/src/Tests/CashMachine.Tests/Tests/BankAccounts/TopUpBalanceTests.cs
@@ -0,0 +1,31 @@
+﻿using CashMachine.Application.Abstractions.Reposituries.Managers;
+using CashMachine.Application.Services.BankAccounts;
+using NSubstitute;
+
+namespace CashMachine.Tests.Tests.BankAccount
+{
+    public class TopUpBalanceTests
+    {
+        [Fact]
+        public void TopUpMoneyTests()
+        {
+            //Arrange
+            var repositoryManager = Substitute.For<IRepositoryManager>();
+            var bankAccountService = new BankAccountService(repositoryManager);
+
+            var bankAccountId = Guid.NewGuid();
+            var initialBalance = 1000;
+            var withdrawalAmount = 500;
+
+            bankAccountService.GetBalance(bankAccountId).Returns(initialBalance);
+
+            // Act
+            var result = bankAccountService.TopUpMoney(bankAccountId, withdrawalAmount); 
+
+            // Assert
+            Assert.Equal(initialBalance + withdrawalAmount, result);
+            // убеждаемся, что метод UpdateBalance был вызван с правильными параметрами
+            repositoryManager.BankAccountRepository.Received(1).UpdateBalance(bankAccountId, initialBalance + withdrawalAmount); 
+        }
+    }
+}
---
+++ CashMachine/src/Tests/CashMachine.Tests/Tests/BankAccounts/WithdrawBalanceTests.cs
@@ -0,0 +1,51 @@
+﻿using CashMachine.Application.Abstractions.Reposituries.Managers;
+using CashMachine.Application.Models.BankAccounts;
+using CashMachine.Application.Services.BankAccounts;
+using NSubstitute;
+
+namespace CashMachine.Tests.Tests.BankAccounts
+{
+    public class WithdrawBalanceTests
+    {
+        [Fact]
+        public void WithdrawMoney_EnoughMoneyTests()
+        {
+            //Arrange
+            var repositoryManager = Substitute.For<IRepositoryManager>();
+            var bankAccountService = new BankAccountService(repositoryManager);
+
+            var bankAccountId = Guid.NewGuid();
+            var initialBalance = 1000;
+            var withdrawalAmount = 500;
+
+            bankAccountService.GetBalance(bankAccountId).Returns(initialBalance);
+
+            // Act
+            var result = bankAccountService.WithdrawMoney(bankAccountId, withdrawalAmount);
+
+            // Assert
+            Assert.Equal(initialBalance - withdrawalAmount, result);
+            // убеждаемся, что метод UpdateBalance был вызван с правильными параметрами
+            repositoryManager.BankAccountRepository.Received(1).UpdateBalance(bankAccountId, initialBalance - withdrawalAmount);
+        }
+
+        [Fact]
+        public void WithdrawMoney_NotEnoughMoneyTests()
+        {
+            // Arrange
+            var repositoryManager = Substitute.For<IRepositoryManager>();
+            var bankAccountService = new BankAccountService(repositoryManager);
+
+            var accountId = Guid.NewGuid();
+            var initialBalance = 100m;
+            var withdrawalAmount = 200m;
+
+            bankAccountService.GetBalance(accountId).Returns(initialBalance);
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => bankAccountService.WithdrawMoney(accountId, withdrawalAmount));
+            repositoryManager.BankAccountRepository.DidNotReceive().UpdateBalance(Arg.Any<Guid>(), Arg.Any<decimal>());
+            repositoryManager.BankAccountHistoryRepository.DidNotReceive().Create(Arg.Any<BankAccountHistory>());
+        }
+    }
+}
